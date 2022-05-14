a595de9c5638f3d36a57d342949b02c909547322
Richard Watts <richard@bigpayme.com>
2022-05-14 22:56:55 +0800

    I can now create a tree memory segment.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use revproc1::utils::memory;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let some_memory = memory::VectorMemory::new();
+    let some_memory = memory::TreeMemory::new();
     println!("Hello, world!");
     Ok(())
 }
--- src/utils/memory.rs
+++ src/utils/memory.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use std::{cmp, vec::Vec};
+use std::{cell::RefCell, cmp, fmt};
 
 #[async_trait]
 pub trait MemoryAccess {
@@ -7,88 +7,82 @@ pub trait MemoryAccess {
     async fn write(&self, loc: u64, contents: &Vec<u64>);
 }
 
-struct MemorySegment {
-    start_address: u64,
-    contents: Vec<u64>,
-}
-
-impl MemorySegment {
-    // Return 1 past the end of the segment.
-    fn limit(&self) -> u64 {
-        self.start_address + (self.contents.len() as u64)
-    }
-    fn bytes(&self) -> u64 {
-        self.contents.len() as u64
-    }
-}
-
-pub struct VectorMemory {
-    /// Values not in a segment read as 0.
-    segments: Vec<MemorySegment>,
-
-    /// Segment size, in log_2 words (so 1MW == 20)
-    chunk_size: u64,
-}
-
 enum MemOp {
     Read,
     Write,
 }
 
-impl VectorMemory {
-    pub fn new() -> VectorMemory {
-        // 1MW chunks (== 16MiB)
-        VectorMemory {
-            segments: vec![],
-            chunk_size: 20,
+enum MemorySegment {
+    Nothing(),
+    Next(RefCell<Vec<MemorySegment>>),
+    Memory(RefCell<Vec<u64>>),
+}
+
+impl fmt::Debug for MemorySegment {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut dbg = f.debug_struct("MemorySegment");
+        match self {
+            MemorySegment::Nothing() => {
+                dbg.field("Nothing", &"nothing");
+                ()
+            }
+            MemorySegment::Next(segment) => {
+                let mut idx = 0;
+                for mem in &*segment.borrow() {
+                    dbg.field("Seg{i}", &mem);
+                    idx += 1;
+                }
+            }
+            MemorySegment::Memory(vec) => {
+                dbg.field("Memory", &vec.borrow().len());
+                ()
+            }
+        }
+        dbg.finish()
+    }
+}
+
+/// A tree memory, populated on demand.
+/// Each level of the tree supplies bits_per_segment bits, and each
+/// element is either a memory array, a pointer to another table, or nothing.
+/// By default we put memory itself at the leaves.
+pub struct TreeMemory {
+    /// Root of the tree.
+    root: RefCell<MemorySegment>,
+    /// Bits per segment - size of the tables
+    bits_per_segment: u32,
+    /// How deep is the tree?
+    max_depth: u32,
+}
+
+impl TreeMemory {
+    pub fn new() -> TreeMemory {
+        TreeMemory {
+            root: RefCell::new(MemorySegment::Nothing()),
+            // 4096 element per array
+            bits_per_segment: 12,
+            // 48 bits => 1MiB segments - a bit small, but ...
+            max_depth: 4,
         }
     }
 
-    /// Ensure that chunks are present for a segment from loc to size
-    async fn ensure_present(&mut self, loc: u64, size: u64) {
-        // Generate a list of segments that should be there.
-        let segs: Vec<u64> = vec![];
-        let first = loc >> chunk_size;
-        let last = ((loc + size) >> chunk_size) + 1;
+    /// Perform an iop against a segment, with a base address and depth
+    fn iop(
+        &self,
+        offset: u64,
+        iovec: &mut Vec<u64>,
+        op: MemOp,
+        segment: &RefCell<MemorySegment>,
+        address: u64,
+        depth: u32,
+    ) {
     }
 
-    /// Read from or write to memory.
-    /// Updates elements of `iovec` and `self.segments` according to
-    /// the operation and addresses.
-    /// Out of range addresses are not touched.
-    async fn iop(&mut self, loc: u64, iovec: &mut Vec<u64>, op: MemOp) {
-        let limit = loc + (iovec.len() as u64);
-        for segment in &mut self.segments {
-            let segment_limit = segment.limit();
-            let copy_start_loc = cmp::max(segment.start_address, loc);
-            let copy_end_loc = cmp::min(segment.limit(), limit);
-            if copy_start_loc > segment_limit {
-                continue;
-            }
-            if copy_end_loc < segment.start_address {
-                continue;
-            }
-            let copy_mem_offset = copy_start_loc - segment.start_address;
-            let copy_vec_offset = copy_start_loc - loc;
-            let copy_nr_bytes = cmp::min(copy_end_loc - copy_start_loc, segment.bytes() as u64);
-            let mem = &mut segment.contents
-                [(copy_mem_offset as usize)..((copy_mem_offset + copy_nr_bytes) as usize)];
-            let iov = &mut iovec
-                [(copy_vec_offset as usize)..((copy_vec_offset + copy_nr_bytes) as usize)];
-            // outside the map for efficiency
-            match op {
-                MemOp::Read => {
-                    for (mem_i, iov_i) in mem.into_iter().zip(iov.iter_mut()) {
-                        *iov_i = *mem_i;
-                    }
-                }
-                MemOp::Write => {
-                    for (mem_i, iov_i) in mem.iter_mut().zip(iov.into_iter()) {
-                        *mem_i = *iov_i;
-                    }
-                }
-            }
-        }
+    fn get_max_address(&self, address: u64, depth: u32) -> u64 {
+        // Compute the base and limit of this segment
+        let bits_in_address = self.bits_per_segment * depth;
+        let mask = ((1 << bits_in_address) - 1) << (64 - bits_in_address);
+        address | (!0 & mask)
     }
 }
 
@@ -96,8 +90,11 @@ impl VectorMemory {
 mod tests {
     use crate::utils::memory;
     #[test]
-    fn create_vector_memory() {
-        let mem = memory::VectorMemory::new();
-        assert_eq!(mem.segments.len(), 0);
+    fn create_memory() {
+        let mem = memory::TreeMemory::new();
+        assert!(matches!(
+            *mem.root.borrow(),
+            memory::MemorySegment::Nothing()
+        ));
     }
 }
